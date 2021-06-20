ccbbcebe51dbf04ef529e246519d1b409f7320bd
Deviant <michaelngo1337@yahoo.com>
2021-06-20 00:30:22 -0700

    added comments to server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+// require statements
 const express = require('express')
 const { join } = require('path')
 
+// instantiate express
 const app = express()
 
+// middleware
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// connect routes
 app.use(require('./routes'))
 
+// listen to port
 app.listen(process.env.PORT || 3000)
